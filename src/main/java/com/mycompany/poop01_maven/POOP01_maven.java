68be7dbfb7c95186fddcf8cb54865027bb5c46ca
LILIAN OSORIO <146885696+LilianOsorio@users.noreply.github.com>
2023-11-02 00:21:13 -0600

    Practica 01 con maven

---
+++ src/main/java/com/mycompany/poop01_maven/POOP01_maven.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop01_maven;
+
+/**
+ *La clase POOP01_maven es la clase principal del programa.
+ * @author lilian
+ */
+public class POOP01_maven {
+     /**
+     * El método main es el punto de entrada principal para la aplicación.
+     * 
+     * @param args Los argumentos de la línea de comandos, si los hay.
+     */
+
+    public static void main(String[] args) {
+        // Imprime un mensaje en la consola
+        System.out.println("Hola mundo desde POO grupo 08 con Maven");
+    }
+}
